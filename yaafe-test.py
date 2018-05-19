1770059aeb81d7cf8a760a9dcdd74a5c2abddc77
Aidan Johnson <34667756+aidanjohnson@users.noreply.github.com>
2018-05-19 16:22:41 -0700

    added ability to play audio

--- yaafe-test.py
+++ yaafe-test.py
@@ -8,9 +8,10 @@ Created on Wed May 16 08:47:24 2018
 
 import numpy as np
 from yaafelib import *
+import pyglet
 import sklearn
 
-audiofile = ''
+audiofile = 'IRMAS-TrainingData/pia/254__[pia][nod][cla]1406__2.wav'
 
 fp = FeaturePlan(sample_rate=16000)
 fp.addFeature('mfcc: MFCC blockSize=512 stepSize=256')
@@ -37,7 +38,10 @@ afp.processFile(engine,audiofile)
 feats = engine.readAllOutputs()
 
 afp.setOutputFormat('csv','output',{'Precision':'8'})
-# afp.processFile(engine,audiofile)
+afp.processFile(engine,audiofile)
 
 audio = np.random.randn(1,100000)
 feats = engine.processAudio(audio)
+
+song = pyglet.media.load(str(audiofile))
+song.play()
