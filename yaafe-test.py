1c9ab6ffcd8823d668400e34ee9c0cb845c7919c
Aidan Johnson <34667756+aidanjohnson@users.noreply.github.com>
2018-05-21 16:33:54 -0700

    recurses; gets features for all in directory
    can now recurse for all wav files in order to get csv of features for each wav file

--- yaafe-test.py
+++ yaafe-test.py
@@ -11,39 +11,46 @@ from yaafelib import *
 import pyglet
 import sklearn
 
-audiofile = '05 Sonatine Bureaucratique_clip.wav'
+from glob import glob
 
-fp = FeaturePlan(sample_rate=44100)
-fp.loadFeaturePlan('featureplan.txt')
-# All the features below are listed line-by-line in featureplan.txt:
-# fp.addFeature('mfcc: MFCC blockSize=512 stepSize=256')
-# fp.addFeature('mfcc_d1: MFCC blockSize=512 stepSize=256 > Derivate DOrder=1')
-# fp.addFeature('mfcc_d2: MFCC blockSize=512 stepSize=256 > Derivate DOrder=2')
-# fp.addFeature('sf: SpectralFlatness blockSize=1024')
-# fp.addFeature('sr: SpectralRolloff blockSize=1024')
-# fp.addFeature('ss: SpectralSlope')
-# fp.addFeature('obsi: OBSI')
-# fp.addFeature('tss: TemporalShapeStatistics')
-# fp.addFeature('zcr: ZCR')
-# fp.addFeature('sv: SpectralVariation')
-# fp.addFeature('sss: SpectralShapeStatistics')
+wavFiles = []
+for file in glob("**/*.wav", recursive = True):
+    wavFiles.append(file)
 
-df = fp.getDataFlow()
-df.display()
+print(wavFiles)
 
-engine = Engine()
-engine.load(df)
-
-afp = AudioFileProcessor()
-afp.processFile(engine,audiofile)
-
-feats = engine.readAllOutputs()
-
-afp.setOutputFormat('csv','output',{'Precision':'8'})
-afp.processFile(engine,audiofile)
-
-# audio = np.random.randn(1,100000)
-# feats = engine.processAudio(audio)
-
-song = pyglet.media.load(str(audiofile))
-song.play()
+#audiofile = '05 Sonatine Bureaucratique_clip.wav'
+for audiofile in wavFiles:
+   fp = FeaturePlan(sample_rate=44100)
+   fp.loadFeaturePlan('featureplan.txt')
+   # All the features below are listed line-by-line in featureplan.txt:
+   # fp.addFeature('mfcc: MFCC blockSize=512 stepSize=256')
+   # fp.addFeature('mfcc_d1: MFCC blockSize=512 stepSize=256 > Derivate DOrder=1')
+   # fp.addFeature('mfcc_d2: MFCC blockSize=512 stepSize=256 > Derivate DOrder=2')
+   # fp.addFeature('sf: SpectralFlatness blockSize=1024')
+   # fp.addFeature('sr: SpectralRolloff blockSize=1024')
+   # fp.addFeature('ss: SpectralSlope')
+   # fp.addFeature('obsi: OBSI')
+   # fp.addFeature('tss: TemporalShapeStatistics')
+   # fp.addFeature('zcr: ZCR')
+   # fp.addFeature('sv: SpectralVariation')
+   # fp.addFeature('sss: SpectralShapeStatistics')
+   
+   df = fp.getDataFlow()
+   df.display()
+   
+   engine = Engine()
+   engine.load(df)
+   
+   afp = AudioFileProcessor()
+   
+   feats = engine.readAllOutputs()
+   
+   afp.setOutputFormat('csv','output_' + audiofile,{'Precision':'8'})
+   afp.processFile(engine,audiofile)
+   
+   # audio = np.random.randn(1,100000)
+   # feats = engine.processAudio(audio)
+   
+   #song = pyglet.media.load(str(audiofile))
+   #song.play()
