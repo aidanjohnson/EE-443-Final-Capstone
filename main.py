5ac70b0859be68c52daf847ff0a8809223634071
Deniz Alpay <31429071+dalpay@users.noreply.github.com>
2018-05-23 22:19:40 -0700

    Only audio with one instrument is used

--- main.py
+++ main.py
@@ -3,35 +3,43 @@ import subprocess
 import yaafelib as yl
 
 def writeTrainFeatures(dataPath, featPath):
-
+    
     featFile = open(featPath, 'w')
-    featFile.write('1234578')   # Place holder for top line that is written last
+    featFile.write('             ') # Place holder for top line that is written last
     totalFrames = 0 # The total number of frames processed
-    dimensions = 18 # The sum of the dimensions of the features
-    instrList = os.listdir(dataPath)
+    instrList = [item for item in os.listdir(dataPath) if not item.endswith('.txt')]
     instrIndex = dict(zip(instrList, range(len(instrList))))
+    limit = 100 # The number of audio files for each instrument that's used
 
     for instr in instrList:
-        for audioFile in os.listdir(os.path.join(dataPath, instr)):
-            
-            # Get features
-            afp.processFile(eng, os.path.join(dataPath, instr, audioFile))
-            feats = eng.readAllOutputs()
-            keys = list(feats.keys())
 
-            # Update the number of frames
-            numFrames = len(feats.get(keys[0]))
-            totalFrames += numFrames
+        count = 0
+        subStr = '[' + instr + ']' + '[nod]'
 
-            # Write features
-            for frInd in range(numFrames):
-                for feat in keys:
-                    for val in feats.get(feat)[frInd]:
-                        featFile.write('%.8f ' % val)
-                
-                featFile.write('%d' % instrIndex.get(instr))
-                featFile.write('\n')
+        for audioFile in (fn for fn in os.listdir(os.path.join(dataPath, instr)) if subStr in fn):
 
+            count += 1
+
+            if (count < limit):
+
+                # Get features
+                afp.processFile(eng, os.path.join(dataPath, instr, audioFile))
+                feats = eng.readAllOutputs()
+                featList = list(feats.keys())
+
+                # Update the number of frames
+                numFrames = len(feats.get(featList[0]))
+                totalFrames += numFrames
+
+                # Write features
+                for frInd in range(numFrames):
+                    for feat in featList:
+                        for val in feats.get(feat)[frInd]:
+                            featFile.write('%.8f ' % val)
+                    
+                    featFile.write('%d' % instrIndex.get(instr))
+                    featFile.write('\n')
+    
     # Write the number of frames and dimensions
     featFile.seek(0)
     featFile.write('%d %d\n' % (totalFrames, dimensions + 1))
@@ -40,39 +48,40 @@ def writeTrainFeatures(dataPath, featPath):
     return instrIndex
 
 
-def writeTestData(dataPath, featPath, instrIndex):
+def writeTestFeatures(dataPath, featPath, instrIndex):
 
     featFile = open(featPath, 'w')
-    featFile.write('1234578')   # Place holder for top line that is written last
+    featFile.write('             ') # Place holder for top line that is written last
     totalFrames = 0 # The total number of frames processed
-    dimensions = 18 # The sum of the dimensions of the features
     
-    for audioFile in (file for file in  os.listdir(dataPath) if file.endswith('.wav')):
+    for audioFile in (fn for fn in os.listdir(dataPath) if fn.endswith('.wav')):
         
         # Get the label
-        substr = audioFile.split('.')
-        filename = ".".join(substr[0 : len(substr) - 1])
+        subStr = audioFile.split('.')
+        filename = ".".join(subStr[0 : len(subStr) - 1])
         labelFile = open(os.path.join(dataPath, filename + '.txt'), 'r')
         label = labelFile.readline().strip()
-        labelFile.close()
+        otherInstr = labelFile.readline()
         
-        # Get features
-        afp.processFile(eng, os.path.join(dataPath, audioFile))
-        feats = eng.readAllOutputs()
-        keys = list(feats.keys())
-
-        # Update the number of frames
-        numFrames = len(feats.get(keys[0]))
-        totalFrames += numFrames
-
-        # Write features
-        for frInd in range(numFrames):
-            for feat in keys:
-                for val in feats.get(feat)[frInd]:
-                    featFile.write('%.8f ' % val)
+        if (not otherInstr.isspace() and label in instrIndex):
             
-            featFile.write('%d' % instrIndex.get(label))
-            featFile.write('\n')
+            # Get features
+            afp.processFile(eng, os.path.join(dataPath, audioFile))
+            feats = eng.readAllOutputs()
+            featList = list(feats.keys())
+
+            # Update the number of frames
+            numFrames = len(feats.get(featList[0]))
+            totalFrames += numFrames
+
+            # Write features
+            for frInd in range(numFrames):
+                for feat in featList:
+                    for val in feats.get(feat)[frInd]:
+                        featFile.write('%.8f ' % val)
+                
+                featFile.write('%d' % instrIndex.get(label))
+                featFile.write('\n')
 
     # Write the number of frames and dimensions
     featFile.seek(0)
@@ -80,19 +89,21 @@ def writeTestData(dataPath, featPath, instrIndex):
     featFile.close()
 
 
-# MAIN
+# Main
 
-trainAudio = './IRMAS-Sample/Training/'
-trainFeats = './trainFeatures.dat'
-testAudio = './IRMAS-Sample/Testing/'
-testFeats = './testFeatures.dat'
-model = 'model.svm'
+# trainAudio = '/home/deniz/Documents/IRMAS/IRMAS-Sample/Training/'
+trainAudio = '/home/deniz/Documents/IRMAS/BinaryTrainingData/'
+trainFeats = './binaryModel/trainFeatures.dat'
+testAudio = '/home/deniz/Documents/IRMAS/TestingData/'
+testFeats = './binaryModel/testFeatures.dat'
+model = './binaryModel/model.svm'
 
 # Specify features
-# Change the feature dimensions value in the functions above if these are changed
 fp = yl.FeaturePlan(sample_rate=44100)
 fp.addFeature('mfcc: MFCC blockSize=1024 stepSize=512 CepsNbCoeffs=13 FFTWindow=Hamming')
-fp.addFeature('lpc: LPC blockSize=1024 stepSize=512 LPCNbCoeffs=5')
+fp.addFeature('lsf: LSF blockSize=1024 stepSize=512 LSFNbCoeffs=7')
+# Dimensions: mfcc +13, lsf +3, sss +4, tss +4
+dimensions = 20 # The sum of the dimensions of the features
 
 # Initialize yaafe tools
 df = fp.getDataFlow()
@@ -101,13 +112,17 @@ eng.load(df)
 afp = yl.AudioFileProcessor()
 
 # Write training features
+print('\nExtracting training features\n')
 instruments = writeTrainFeatures(trainAudio, trainFeats)
 
 # Train the svm 
+print('\nTraining SVM\n')
 subprocess.call(['./trainSVM', '-multi', trainFeats, model])
 
 # Write cross-validation features
-writeTestData(testAudio, testFeats, instruments)
+print('\nExtracting testing features\n')
+writeTestFeatures(testAudio, testFeats, instruments)
 
 # Cross-validate the model
+print('\nTesting SVM\n')
 subprocess.call(['./testSVM', '-multi', model, testFeats])
