439871b1f81ec76e57477960dda5d025e8c4d730
Deniz Alpay <31429071+dalpay@users.noreply.github.com>
2018-05-25 12:11:02 -0700

    Only uses classical instruments now

--- main.py
+++ main.py
@@ -4,10 +4,19 @@ import yaafelib as yl
 
 
 def getInstruments(dataPath):
+
     # Instruments to ignore
-    ignInstr = ['voi', 'gel', 'gac', 'pia', 'org', 'tru'] 
+    ignInstr = ['voi', 'gel', 'gac', 'org', 'tru', 'flu'] 
+
+    # This leaves cel, cla, pia, sax, vio
+    ignInstr.append('pia')
+    ignInstr.append('vio') 
+    ignInstr.append('sax')
+
+    # Assign class indices of the instruments
     instrList = [n for n in os.listdir(dataPath) if not n.endswith('.txt') and n not in ignInstr]
     instrIndex = dict(zip(instrList, range(len(instrList))))
+    
     return instrIndex
 
 
@@ -16,12 +25,12 @@ def writeTrainFeatures(dataPath, featPath, instrIndex):
     featFile = open(featPath, 'w')
     featFile.write('             ') # Place holder for top line that is written last
     totalFrames = 0 # The total number of frames processed
-    limit = 60      # The number of audio files for each instrument that's used
+    limit = 60     # The number of audio files for each instrument that's used
 
     for instr in (fn for fn in instrIndex.keys() if not fn.endswith('.txt')):
 
         count = 0
-        subStr = '[' + instr + ']' + '[nod]'
+        subStr = '[' + instr + ']' + '[nod][cla]'
 
         for audioFile in (fn for fn in os.listdir(os.path.join(dataPath, instr)) if subStr in fn):
             if (count < limit):
@@ -97,11 +106,11 @@ def writeTestFeatures(dataPath, featPath, instrIndex):
 # Main
 
 # trainAudio = '/home/deniz/Documents/IRMAS/IRMAS-Sample/Training/'
-trainAudio = '/home/deniz/Documents/IRMAS/BinaryTrainingData/'
-trainFeats = './BinaryModel/trainFeatures.dat'
+trainAudio = '/home/deniz/Documents/IRMAS/TrainingData/'
+trainFeats = './MultiModel/trainFeatures.dat'
 testAudio = '/home/deniz/Documents/IRMAS/TestingData/'
-testFeats = './BinaryModel/testFeatures.dat'
-model = './BinaryModel/model.svm'
+testFeats = './MultiModel/testFeatures.dat'
+model = './MultiModel/model.svm'
 
 # Get the instruments
 instruments = getInstruments(trainAudio)
@@ -109,15 +118,18 @@ instruments = getInstruments(trainAudio)
 # Specify features
 fp = yl.FeaturePlan(sample_rate=44100)
 fp.addFeature('mfcc: MFCC blockSize=1024 stepSize=512 CepsNbCoeffs=13 FFTWindow=Hamming')
-# fp.addFeature('lsf: LSF blockSize=1024 stepSize=512 LSFNbCoeffs=3')
-# Dimensions: mfcc +13, lsf +3, sss +4, tss +4
-dimensions = 13 # The sum of the dimensions of the features
+fp.addFeature('sss: SpectralShapeStatistics blockSize=1024 stepSize=512 FFTWindow=Hamming')
+# fp.addFeature('obsi: OBSI blockSize=1024 stepSize=512 FFTWindow=Hamming')
+# Dimensions: mfcc +13, sss +4, obsi +10
+dimensions = 17 # The sum of the dimensions of the features
 
 # Initialize yaafe tools
 df = fp.getDataFlow()
 eng = yl.Engine()
 eng.load(df)
 afp = yl.AudioFileProcessor()
+
+# Remove previous model files
 
 # Write training features
 print('\nExtracting training features\n')
