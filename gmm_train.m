20bb294e6392b2411069f63ab9081ea40daa5985
Deniz Alpay <31429071+dalpay@users.noreply.github.com>
2018-06-03 10:59:54 -0700

    New training features

--- gmm_train.m
+++ gmm_train.m
@@ -17,36 +17,36 @@ sss = zeros(1, 4);
 fid = fopen(featPath, 'w');
 for l = 1:length(instr)
 
-    fprintf('Extractin features for %s\n', instr{l});
-    audioFiles = dir(fullfile(dataPath, instr{l}, '*.wav'));
+    fprintf('\nExtracting features for %s\n', instr{l});
+    fileFormat = ['*[', instr{l}, '][nod]*.wav'];
+    audioFiles = dir(fullfile(dataPath, instr{l}, fileFormat));
 
     for k = 1:length(audioFiles)
-        if (contains(audioFiles(k).name, '[nod]'))
-            % Process files
-            filename = audioFiles(k).name;
-            fprintf('Processing %s\n', filename);
-            [audio, fs] = audioread(fullfile(audioFiles(k).folder, audioFiles(k).name));
+
+        % Process files
+        filename = audioFiles(k).name;
+        fprintf('Processing %s (%d of %d)\n', filename, k, length(audioFiles));
+        [audio, fs] = audioread(fullfile(audioFiles(k).folder, audioFiles(k).name));
+        
+        % Compute features for each frame
+        for n = 1:floor(length(audio)/len)
+
+            frame = audio((n - 1)*len + 1 : n*len);
+            mag = abs(fft(frame));
+            energy = sum(frame.^2);
             
-            % Compute features for each frame
-            for n = 1:floor(length(audio)/len)
-                
-                frame = audio((n - 1)*len + 1 : n*len);
-                mag = abs(fft(frame));
-                
-                % MFCC
-                for m = 1:numCoeff
-                    mfcc(m) = GetCoefficient(mag, fs, 48, len, m);
-                end
-                
-                % Shape statistics
-                sss = GetShapeStatistics(mag);
-                
-                feats = [mfcc, sss];
-                fprintf(fid, '%.8f ', feats);
-                fprintf(fid, '\n');
+            % MFCC
+            for m = 1:numCoeff
+                mfcc(m) = GetCoefficient(mag, fs, numFilter, len, m);
             end
-        else
-            fprintf('Ignored %s\n', audioFiles(k).name);
+            
+            % Shape statistics
+            avg = mean(frame);
+            sss = GetShapeStatistics(mag/energy);
+            
+            feats = [mfcc, sss];
+            fprintf(fid, '%.8f ', feats);
+            fprintf(fid, '\n');
         end
     end
 end
